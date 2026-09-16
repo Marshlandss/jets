3006e863269a81a9acc1e7afd2f4ab01b457447e
Martijn Simon Soen Liong Oei <martijnoei@gmail.com>
2026-09-16 03:28:18 -0700

    Draft BORG SDSS mean filament orientations script

--- scripts/find_filament_orientations_borg_sdss_mean.py
+++ scripts/find_filament_orientations_borg_sdss_mean.py
@@ -1,9 +1,38 @@
+"""
+Find the Cosmic Web filament orientations of Mpc-scale jet systems, using the BORG SDSS posterior mean density cube.
+"""
 # Imports: third-party
 import numpy as np
-import pandas as pd
 # Imports: first-party
-from jets.filament_orientation import FilamentOrientationFinder
+from jets.filament_orientation import FilamentOrientationFinder, findFilamentOrientations, writeFilamentOrientations, loadVoxelIndices
+from jets.paths import DIR_LOAD, DIR_SAVE
 
+# Initialise settings.
+lambdaMax     = 2.5 # in BORG voxel side lengths
+stepAngle     = 1.  # in deg
+methods       = ["d", "a"] # "d": direct, "a": adjusted
+
+# Initialise paths.
+pathDensities = DIR_LOAD / "borg_sdss" / "borg_sdss_density.npz"
+pathExcel     = DIR_SAVE / "Mpc_filament_pa_exact_1.xlsx"
+
+# Initialise filament orientation finding.
+FOF           = FilamentOrientationFinder(lambdaMax, stepAngle)
+
+# Load the BORG SDSS posterior mean.
+densitiesMean = np.load(pathDensities)["mean"] + 1 # in today's mean matter density
+print(f"Loaded density cube of shape {densitiesMean.shape}; mean {np.mean(densitiesMean):.3f}, range [{np.amin(densitiesMean):.3f}, {np.amax(densitiesMean):.3f}].")
+
+for method in methods:
+    print(f"Finding filament orientations for method '{method}'...")
+    voxelIndicesList = loadVoxelIndices(pathExcel, method)
+    columnDensitiesAll, azimuthsBest, altitudesBest, columnDensitiesBest = findFilamentOrientations(FOF, densitiesMean, voxelIndicesList)
+
+    np.save(DIR_SAVE / f"Mpc_column_densities_all_{method}.npy", columnDensitiesAll)
+    writeFilamentOrientations(pathExcel, azimuthsBest, altitudesBest, columnDensitiesBest, method)
+
+
+'''
 directoryDataBORGSDSS  = "/Users/martijnoei/Library/CloudStorage/Dropbox-Personal/Martijn/PhD/Ardor Telae/data/BORG SDSS/"
 
 # Initialise FOF.
@@ -47,3 +76,4 @@ for i in range(10):
     plt.show()
 FOF.writeNumPy(directoryExcel + "Mpc_column_densities_all_j.npy")
 #FOF.write(pathExcel, methodDirect = False)
+'''
