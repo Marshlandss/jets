8d2d9f7ece785bc3542a9765911abd85933046a6
Martijn Simon Soen Liong Oei <martijnoei@gmail.com>
2026-09-18 23:01:29 -0700

    Uniformize units

--- src/jets/config.py
+++ src/jets/config.py
@@ -2,21 +2,21 @@
 SEED = 0
 
 # Set jet orientation search parameters.
-JET_ANGLE_STEP        = 1.   # in deg; angular resolution of the jet orientation search
+JET_ANGLE_STEP        = 1.  # in deg; angular resolution of the jet orientation search
 BEST_ANGLE_THRESHOLD  = .85
-PIXEL_SHIFT           = 1    # shifts plot by indicated number of pixels to the top right; Martijn: 0, Martin: 1
-NAN_PERCENTAGE_CUTOFF = 43.  # in %
+PIXEL_SHIFT           = 1   # shifts plot by indicated number of pixels to the top right; Martijn: 0, Martin: 1
+NAN_PERCENTAGE_CUTOFF = 43. # in %
 
 # Set cosmology and BORG SDSS parameters.
-LITTLE_H              = 0.702                                                    # in 1; H_0 / (100 km s^-1 Mpc^-1), matching Jasche et al. (12015)
+LITTLE_H              = 0.702                                                    # in 1; H_0 / (100 km/s/Mpc), matching Jasche et al. (12015)
 DENSITY_MEAN_TODAY    = 2.6e-24                                                  # in g/m^3
-BORG_BOX_SIZE_MPC_H   = 750.                                                     # in Mpc / h; comoving
+BORG_BOX_SIZE_MPC_H   = 750.                                                     # in Mpc/h; comoving
 BORG_NUMBER_OF_VOXELS = 256                                                      # in 1; per axis
 BORG_VOXEL_SIZE_MPC   = BORG_BOX_SIZE_MPC_H / (BORG_NUMBER_OF_VOXELS * LITTLE_H) # in Mpc (≈ 4.17); comoving
 
 # Set beta-profile filament parameters from Tuominen et al. (12021), used for voxelisation error simulations.
 FILAMENT_BETA            = 2.0     # in 1
-FILAMENT_DENSITY_CENTRAL = 1.6e-23 # in g m^-3
+FILAMENT_DENSITY_CENTRAL = 1.6e-23 # in g/m^3
 FILAMENT_RADIUS_CORE     = 1.2     # in Mpc
 
 # Set filament orientation search parameters.
